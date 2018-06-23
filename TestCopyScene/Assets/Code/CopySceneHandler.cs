d653047602f9496fd3abdc4b1e3261b3cf5b0aff
zhangguobing <184624026@qq.com>
2018-06-23 12:01:35 +0800

    xx

---
+++ TestCopyScene/Assets/Code/CopySceneHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CopySceneHandler : MonoBehaviour
+{
+	public List<string> list = new List<string>();
+
+	[SerializeField]
+	private TextAsset _map;
+
+	public TextAsset map {
+		set{
+			_map = value;
+		}
+		get{
+			return _map;
+		}
+
+	}
+
+	void ToStudio()
+	{
+//		CopySceneToStudio(BranchCommand.GetStudioPath(), "Studio");
+	}
+
+	void CopySceneToStudio(string path,string name)
+	{
+
+	}
+}
+
+
---
+++ TestCopyScene/Assets/Editor/CopySceneInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CopySceneHandler))]
+public class CopySceneInspector : Editor
+{
+
+
+	int selected = 0;
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI(); 
+
+		//		(EnumList)EditorGUILayout.EnumPopup("EnumPopup", enumValue);
+		EditorGUILayout.TextField("地图路径","Assets/Resource/Script/FrameWork/Config/Table_Map.txt",EditorStyles.textField);
+		CopySceneHandler obj = target as CopySceneHandler;
+		string [] list = {"Studio","TF","Release"};
+		selected = EditorGUILayout.Popup("拷贝分支",selected,list,EditorStyles.popup);
+		if (GUILayout.Button("开始拷贝"))  
+		{  
+			CopySceneHandler tar = target as CopySceneHandler;
+		}
+	}
+}
+
